e8987f50c6046e874ed4f5ba6792eb6bcc4bc56a
Sally Northmore <sally.northmore@gmail.com>
2024-06-28 10:48:07 -0400

    Create blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,36 +1,42 @@
 import * as React from 'react'
-import type { HeadFC, PageProps } from "gatsby"
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
-import Seo from '../components/Seo'
+import Seo from '../components/seo'
 
-const BlogPage: React.FC<PageProps> = ({ data })  => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
-
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
       }
     }
   }
 `
 
-export const Head: HeadFC =  () => <Seo title="My Blog Posts" />
+export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
